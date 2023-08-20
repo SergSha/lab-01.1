471db1dac0c7d274dfdcf86890f2f47b5d73d7c6
SergSha <sy.shagalov@yandex.ru>
2023-08-21 00:02:20 +0300

    add all files

---
+++ main.tf
@@ -0,0 +1,67 @@
+locals {
+  user            = "debian"
+  ssh_public_key  = file("~/.ssh/otus.pub")
+  ssh_private_key = file("~/.ssh/otus")
+}
+
+resource "yandex_vpc_network" "vpc" {
+  # folder_id = var.folder_id
+  name = var.vpc_name
+}
+
+resource "yandex_vpc_subnet" "subnet" {
+  # folder_id = var.folder_id
+  v4_cidr_blocks = var.subnet_cidrs
+  zone           = var.zone
+  name           = var.subnet_name
+  network_id     = yandex_vpc_network.vpc.id
+}
+
+resource "yandex_compute_instance" "instance" {
+  count = 3
+
+  name        = "${var.vm_name}-${count.index}"
+  hostname    = "${var.vm_name}-${count.index}"
+  platform_id = var.platform_id
+  zone        = var.zone
+  # folder_id   = var.folder_id
+  resources {
+    cores         = var.cpu
+    memory        = var.memory
+    core_fraction = var.core_fraction
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.image_id
+      size     = var.disk
+      type     = var.disk_type
+    }
+  }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.subnet.id
+    nat                = var.nat
+    ip_address         = var.internal_ip_address
+    nat_ip_address     = var.nat_ip_address
+  }
+
+  metadata = {
+    ssh-keys           = "${local.user}:${local.ssh_public_key}"
+  }
+
+  provisioner "remote-exec" {
+    inline = ["echo 'Wait until SSH is ready'"]
+
+    connection {
+      host        = yandex_compute_instance.instance[count.index].network_interface[0].nat_ip_address
+      type        = "ssh"
+      user        = local.user
+      private_key = local.ssh_private_key
+    }
+  }
+
+  provisioner "local-exec" {
+    command = "ansible-playbook -u '${local.user}' --private-key '${local.ssh_private_key}' --become -i '${yandex_compute_instance.instance[count.index].network_interface.0.nat_ip_address},' provision.yml"
+  }
+}
